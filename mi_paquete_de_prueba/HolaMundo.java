f78c35c6dc49b6bc72d485c8eb654bce22915555
Lucneonct <lucasalvarez556@gmail.com>
2023-03-14 18:24:30 -0300

    Segundo y tercer ejercicio de Java

--- mi_paquete_de_prueba/HolaMundo.java
+++ mi_paquete_de_prueba/HolaMundo.java
@@ -2,8 +2,23 @@ package mi_paquete_de_prueba;
 
 public class HolaMundo {
 	public static void main(String[] args) {
-  	String nombre;
-  	boolean bandera;
-  	char letra;
-  }
+		int numero = 48;
+		double decimales = 3.55;
+		boolean bandera = false;
+
+		// Consola
+		System.out.println(numero);
+
+		// Añadir 1 a la variable
+		numero++;
+		System.out.println(numero);
+
+		// Añadir 10 a la variable
+		numero += 10;
+		System.out.println(numero);
+
+		// Comparación
+		bandera = numero == 59;
+		System.out.println(bandera);
+	}
 }
